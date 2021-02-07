b6b7bbced83e44eaeb098682cedc7a85dc6f56d0
felipe <felipegabriel@rocketmail.com>
2021-02-07 19:55:01 -0300

    Inicado layout do site

---
+++ src/components/Photo.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="lightbox" @click.self="closeLightbox">
+        <img :src="photoUrl(photo.src)">
+        <div class="lightbox-info">
+        <div class="lightbox-info-inner">
+            Info
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import photos from '@/photos.json';
+export default {
+    name: 'Photo',
+    data() {
+        return {
+            photos,
+        };
+    },
+    computed: {
+        photo() {
+            return this.photos.find((photo) => {
+            return photo.id === Number(this.$route.params.id);
+            });
+        },
+    },
+    methods: {
+        photoUrl(filename) {
+            return require(`../${filename}`);
+        },
+        closeLightbox() {
+        this.$router.push('/');
+        }
+    },
+};
+</script>
+<style>
+    .lightbox {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.8);
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-gap: 2rem;
+    }
+    .lightbox img {
+      margin: auto;
+      width: 100%;
+      grid-column-start: 2;
+    }
+    .lightbox-info {
+      margin: auto 2rem auto 0;
+    }
+    .lightbox-info-inner {
+      background-color: #FFFFFF;
+      display: inline-block;
+      padding: 2rem;
+    }
+  </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="header">
+      <img src="../assets/header.jpg" alt="">
+      <div class="list-menu">
+        <ul>
+          <li>Externos</li>
+          <li>Internos</li>
+          <li>Sobre mim</li>
+          <li>Contato</li>
+        </ul>
+      </div>
+    </div>
+    <Gallery />
+  </div>  
+</template>
+<script>
+import Gallery from '@/components/Gallery.vue';
+export default {
+  name: 'home',
+  components: {
+    Gallery,
+  },
+};
+</script>
+
+<style>
+/* Clam Shell
+#D1BBB0
+
+Rock
+#4C3730
+
+Roman Coffee
+#825E4A
+
+Nevada
+#576163 */
+  @import url('https://fonts.googleapis.com/css2?family=Tangerine');
+.header{
+  width: 100%;
+}
+.header img{
+  width: 94%;
+  object-fit: cover;
+  margin: 0 3%;
+  height: 22vw;
+}
+.header .list-menu{
+  display: inline-flex;
+  list-style: none;
+  width:100%;
+  
+}
+.header .list-menu ul{
+  display: flex;
+  list-style: none;
+  margin: 0 auto;
+  border-top: 1px solid #D1BBB0;
+  border-bottom: 1px solid #D1BBB0;
+  width: 94%;
+}
+.header .list-menu li{
+  color: #825E4A;
+  padding: 0 1rem;
+  line-height: 3rem;
+  cursor: pointer;
+  flex: 1;
+  text-align: center;
+  font-weight: bold;
+  font-family: 'Tangerine';
+  font-size: 2rem;
+}
+.header .list-menu li:hover{
+  background-color: #D1BBB0;
+  color: #f7f1ed;
+}
+</style>
